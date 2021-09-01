43525b7a24ef242926ac419ea8e9bedc35e4156f
lfierz <leonie.fierz@sensirion.com>
2021-09-01 14:21:26 +0200

    adding driver generator output

---
+++ examples/exampleUsage/exampleUsage.ino
@@ -0,0 +1,115 @@
+/*
+ * I2C-Generator: 0.2.0
+ * Yaml Version: 0.1.0
+ * Template Version: 0.7.0-38-g217adaf
+ */
+/*
+ * Copyright (c) 2021, Sensirion AG
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * * Neither the name of Sensirion AG nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <Arduino.h>
+#include <SensirionI2CSdp.h>
+#include <Wire.h>
+
+SensirionI2CSdp sdp;
+
+// TODO: DRIVER_GENERATOR Add missing commands and make printout more pretty
+
+void setup() {
+
+    Serial.begin(115200);
+    while (!Serial) {
+        delay(100);
+    }
+
+    Wire.begin();
+
+    uint16_t error;
+    char errorMessage[256];
+
+    sdp.begin(Wire);
+
+    uint32_t productNumber;
+    uint8_t serialNumber[8];
+    uint8_t serialNumberSize = 8;
+
+    error = sdp.readProductIdentifier(productNumber, serialNumber,
+                                      serialNumberSize);
+
+    if (error) {
+        Serial.print("Error trying to execute readProductIdentifier(): ");
+        errorToString(error, errorMessage, 256);
+        Serial.println(errorMessage);
+    } else {
+        Serial.print("ProductNumber:");
+        Serial.print(productNumber);
+        Serial.print("\t");
+        Serial.print("SerialNumber:");
+        for (size_t i = 0; i < serialNumberSize; i++) {
+            Serial.print(serialNumber[i]);
+            Serial.print(", ");
+        }
+        Serial.println();
+    }
+
+    // Start Measurement
+}
+
+void loop() {
+    uint16_t error;
+    char errorMessage[256];
+
+    // TODO: DRIVER_GENERATOR Adjust measurement delay
+    delay(1000);
+    // TODO: DRIVER_GENERATOR Add scale and offset to printed measurement values
+    // Read Measurement
+
+    int16_t differentialPressure;
+    int16_t temperature;
+    int16_t scalingFactor;
+
+    error =
+        sdp.readMeasurement(differentialPressure, temperature, scalingFactor);
+
+    if (error) {
+        Serial.print("Error trying to execute readMeasurement(): ");
+        errorToString(error, errorMessage, 256);
+        Serial.println(errorMessage);
+    } else {
+        Serial.print("DifferentialPressure:");
+        Serial.print(differentialPressure);
+        Serial.print("\t");
+        Serial.print("Temperature:");
+        Serial.print(temperature);
+        Serial.print("\t");
+        Serial.print("ScalingFactor:");
+        Serial.println(scalingFactor);
+    }
+}
